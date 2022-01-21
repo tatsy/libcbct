dd700ab750c72e7601e567ddcf48ca88e54cfac1
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2022-01-21 18:27:49 +0900

    Add image sequence importer.

--- src/Utils/Volume.h
+++ src/Utils/Volume.h
@@ -5,6 +5,8 @@
 #ifndef LIBCBCT_VOLUME_H
 #define LIBCBCT_VOLUME_H
 
+#include "Common/Logging.h"
+
 template <typename T>
 class Volume {
 public:
@@ -20,30 +22,93 @@ public:
         std::memcpy(data, other.data, sizeof(T) * sizeX * sizeY * sizeZ);
     }
 
-    ~Volume() {
+    Volume(Volume<T> &&other)
+        : sizeX(other.sizeX)
+        , sizeY(other.sizeY)
+        , sizeZ(other.sizeZ)
+        , data(other.data) {
+        other.sizeX = 0;
+        other.sizeY = 0;
+        other.sizeZ = 0;
+        other.data = NULL;
+    }
+
+    virtual ~Volume() {
         delete[] data;
     }
 
-    T &operator(int x, int y, int z) {
-        LIBCBCT_ASSERT(x >= 0 && x < sizeX && y >= 0 && y < sizeY && z >= 0 && z < sizeZ, "Volume index out of bounds!");
-        return data[(z * sizeY + y) * sizeX + x];
+    Volume &operator=(const Volume<T> &other) {
+        sizeX = other.sizeX;        
+        sizeY = other.sizeY;
+        sizeZ = other.sizeZ;
+        this->alloc();
+        std::memcpy(data, other.data, sizeof(T) * sizeX * sizeY * sizeZ);
+        return *this;
     }
 
-    T operator(int x, int y, int z) const {
+    Volume &operator=(Volume<T> &&other) {
+        sizeX = other.sizeX;
+        sizeY = other.sizeY;
+        sizeZ = other.sizeZ;
+        data = other.data;
+        
+        other.sizeX = 0;
+        other.sizeY = 0;
+        other.sizeZ = 0;
+        other.data = NULL;
+
+        return *this;
+    }
+
+    T &operator()(int x, int y, int z) {
         LIBCBCT_ASSERT(x >= 0 && x < sizeX && y >= 0 && y < sizeY && z >= 0 && z < sizeZ, "Volume index out of bounds!");
-        return data[(z * sizeY + y) * sizeX + x];
+        return data[((uint64_t)z * sizeY + (uint64_t)y) * sizeX + (uint64_t)x];
+    }
+
+    T operator()(int x, int y, int z) const {
+        LIBCBCT_ASSERT(x >= 0 && x < sizeX && y >= 0 && y < sizeY && z >= 0 && z < sizeZ, "Volume index out of bounds!");
+        return data[((uint64_t)z * sizeY + (uint64_t)y) * sizeX + (uint64_t)x];
+    }
+
+    T *const ptr() const {
+        return data;
+    }
+
+    template <int D>
+    uint64_t size() const;
+
+    template <>
+    uint64_t size<0>() const {
+        return sizeX;
+    }
+
+    template <>
+    uint64_t size<1>() const {
+        return sizeY;
+    }
+
+    template <>
+    uint64_t size<2>() const {
+        return sizeZ;
     }
 
 private:
     void alloc() {
+        if (data != NULL) {
+            delete[] data;
+        }
+
         if (sizeX * sizeY * sizeZ != 0) {
             data = new T[sizeX * sizeY * sizeZ];
             std::memset(data, 0, sizeof(T) * sizeX * sizeY * sizeZ);
         }
     }
 
-    int sizeX, sizeY, sizeZ;
+    uint64_t sizeX, sizeY, sizeZ;
     T *data = NULL;
 };
+
+using UInt16Volume = Volume<uint16_t>;
+using FloatVolume = Volume<float>;
 
 #endif  // LIBCBCT_VOLUME_H
