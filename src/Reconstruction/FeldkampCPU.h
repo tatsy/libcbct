03c3634a03975501cbcdb4c6b49780fddf483778
Tatsuya Yatagawa <tatsy.mail@gmail.com>
2025-10-31 17:42:02 +0900

    Minor update.

--- src/Reconstruction/FeldkampCPU.h
+++ src/Reconstruction/FeldkampCPU.h
@@ -1,15 +1,15 @@
 #ifdef _MSC_VER
-  #pragma once
+#pragma once
 #endif
 
 #ifndef LIBCBCT_FELDKAMP_CPU_H
-  #define LIBCBCT_FELDKAMP_CPU_H
+#define LIBCBCT_FELDKAMP_CPU_H
 
-  #include "ReconstructionBase.h"
+#include "ReconstructionBase.h"
 
 class LIBCBCT_API FeldkampCPU : public ReconstructionBase {
 public:
-    FeldkampCPU(RampFilter filter = RampFilter::RamLak)
+    FeldkampCPU(RampFilter filter = RampFilter::SheppLogan)
         : ReconstructionBase()
         , filter(filter) {
     }
